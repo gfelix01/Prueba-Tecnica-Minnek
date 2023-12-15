33a0adf06c49e130d04fc10ca37a75753345ad7c
Gabriel Arturo Felix Paez <gabriel_arturo01@hotmail.com>
2023-12-15 18:54:55 -0400

    update Prueba1.js

--- Prueba1.js
+++ Prueba1.js
@@ -1,27 +1,27 @@
-function reverseArrayWithSpecialChars(arr) {
-    //  Array de caracteres especiales y sus índices correspondientes
-    const specialChars = arr.reduce((acc, el, index) => {
-        if (typeof el === 'string' && !el.match(/[a-zA-Z0-9]/)) {
-            acc.push({ index, char: el });
-        }
-        return acc;
-    }, []);
 
-    // Para filtrar los caracteres especiales del array:
-    const arrFiltered = arr.filter(el => typeof el === 'number' || el.match(/[a-zA-Z0-9]/));
+function invertirArray (arr) {
+ 
+    const especiales = arr.reduce((acc, el, index) => {
+       if (typeof el === 'string' && !el.match(/[a-zA-Z0-9]/)) {
+         acc.push({ indice: index, caracter: el })
+       }
+       return acc
+    }, [])
+   
+ 
+    const arrFiltrado = arr.filter(el => typeof el === 'number' || el.match(/[a-zA-Z0-9]/))
+   
+   
+    arrFiltrado.reverse()
+   
 
-    // Invertir el array filtrado
-    arrFiltered.reverse();
-
-    // Colocar los caracteres especiales en sus posiciones originales:
-    specialChars.forEach(({ index, char }) => {
-        arrFiltered.splice(index, 0, char);
-    });
-
-    return arrFiltered;
-}
-
-
-const arr = ['n', 2, '&', 'a', 'l', 9, '$', 'q', 47, 'i', 'a', 'j', 'b', 'z', '%', 8];
-const reversedArr = reverseArrayWithSpecialChars(arr);
-console.log(reversedArr);
+    especiales.forEach(({ indice, caracter }) => {
+       arrFiltrado.splice(indice, 0, caracter)
+    })
+   
+    return arrFiltrado
+   }
+   
+   const arr = ['n', 2, '&', 'a', 'l', 9, '$', 'q', 47, 'i', 'a', 'j', 'b', 'z', '%', 8]
+   const arrInvertido = invertirArray(arr)
+   console.log(arrInvertido)
