08f95238f9b890526487daeac473d7825df2fef2
Gabriel Arturo Felix Paez <gabriel_arturo01@hotmail.com>
2023-12-19 17:22:21 -0400

    Update code according to instruction-tuning and RLHF specifications.

--- Prueba-01/Prueba1.js
+++ Prueba-01/Prueba1.js
@@ -1,28 +1,22 @@
-
 function invertirArray (arr) {
- 
-    const especiales = arr.reduce((acc, el, index) => {
-       if (typeof el === 'string' && !el.match(/[a-zA-Z0-9]/)) {
-         acc.push({ indice: index, caracter: el })
-       }
-       return acc
-    }, [])
-   
- 
-    const arrFiltrado = arr.filter(el => typeof el === 'number' || el.match(/[a-zA-Z0-9]/))
-   
-   
-    arrFiltrado.reverse()
-   
+  const especiales = arr.reduce((acc, el, index) => {
+    if (typeof el === 'string' && !el.match(/[a-zA-Z0-9]/)) {
+      acc.push({ indice: index, caracter: el })
+    }
+    return acc
+  }, [])
 
-    especiales.forEach(({ indice, caracter }) => {
-       arrFiltrado.splice(indice, 0, caracter)
-    })
-   
-    return arrFiltrado
-   }
-   
-   const arr = ['n', 2, '&', 'a', 'l', 9, '$', 'q', 47, 'i', 'a', 'j', 'b', 'z', '%', 8]
-   const arrInvertido = invertirArray(arr)
-   console.log(arrInvertido)
-   
+  const arrFiltrado = arr.filter(el => typeof el === 'number' || el.match(/[a-zA-Z0-9]/))
+
+  arrFiltrado.reverse()
+
+  especiales.forEach(({ indice, caracter }) => {
+    arrFiltrado.splice(indice, 0, caracter)
+  })
+
+  return arrFiltrado
+}
+
+const arr = ['n', 2, '&', 'a', 'l', 9, '$', 'q', 47, 'i', 'a', 'j', 'b', 'z', '%', 8]
+const arrInvertido = invertirArray(arr)
+console.log(arrInvertido)
--- Prueba-02/js/crud.js
+++ Prueba-02/js/crud.js
@@ -1,80 +1,76 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const searchInput = document.getElementById('searchInput');
-    const dogTable = document.getElementById('dogTable');
-    const sortSelect = document.getElementById('sortSelect');
-    const returnButton = document.getElementById('returnButton');
-    let dogsData = [];
+  const searchInput = document.getElementById('searchInput')
+  const dogTable = document.getElementById('dogTable')
+  const sortSelect = document.getElementById('sortSelect')
+  const returnButton = document.getElementById('returnButton')
+  let dogsData = []
 
-    function loadDogData() {
-        fetch('https://dog.ceo/api/breeds/list/all')
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then(data => {
-                const breeds = data.message;
+  function loadDogData () {
+    fetch('https://dog.ceo/api/breeds/list/all')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok')
+        }
+        return response.json()
+      })
+      .then(data => {
+        const breeds = data.message
 
-                dogsData = Object.keys(breeds).map(breed => ({ breed, subBreeds: breeds[breed] }));
+        dogsData = Object.keys(breeds).map(breed => ({ breed, subBreeds: breeds[breed] }))
 
-                displayTable(dogsData);
-            })
-            .catch(error => console.log(error));
-    }
+        displayTable(dogsData)
+      })
+      .catch(error => console.log(error))
+  }
 
-    function displayTable(data) {
-        let tableContent = '<tr><th>Raza</th><th>Subrazas</th></tr>';
-        data.forEach(dog => {
-            tableContent += `<tr><td>${dog.breed}</td><td>${dog.subBreeds.length ? dog.subBreeds.join(', ') : '-'}</td></tr>`;
-        });
-        dogTable.innerHTML = tableContent;
-    }
+  function displayTable (data) {
+    let tableContent = '<tr><th>Raza</th><th>Subrazas</th></tr>'
+    data.forEach(dog => {
+      tableContent += `<tr><td>${dog.breed}</td><td>${dog.subBreeds.length ? dog.subBreeds.join(', ') : '-'}</td></tr>`
+    })
+    dogTable.innerHTML = tableContent
+  }
 
-    loadDogData();
+  loadDogData()
 
-    searchInput.addEventListener('input', function () {
-        const searchText = searchInput.value.toLowerCase();
-        const filteredDogs = dogsData.filter(dog => {
-            
-            return (
-                dog.breed.toLowerCase().includes(searchText) ||
+  searchInput.addEventListener('input', function () {
+    const searchText = searchInput.value.toLowerCase()
+    const filteredDogs = dogsData.filter(dog => {
+      return (
+        dog.breed.toLowerCase().includes(searchText) ||
                 dog.subBreeds.some(subBreed => subBreed.toLowerCase().includes(searchText))
 
-            );
-        });
-        displayTable(filteredDogs);
-    });
-    
-    function sortDataBy(type, order = 'asc') {
-        const sortOrder = order === 'desc' ? -1 : 1;
-    
-        if (type === 'name') {
-            dogsData.sort((a, b) => {
-                return sortOrder * a.breed.localeCompare(b.breed);
-            });
-        } else if (type === 'subBreed') {
-            dogsData.sort((a, b) => {
-                if (a.subBreeds.length === b.subBreeds.length) {
-                    return sortOrder * a.breed.localeCompare(b.breed); // Ordena por nombre si la cantidad de subrazas es la misma
-                }
-                return sortOrder * (a.subBreeds.length - b.subBreeds.length); // Ordena por cantidad de subrazas
-            });
+      )
+    })
+    displayTable(filteredDogs)
+  })
+
+  function sortDataBy (type, order = 'asc') {
+    const sortOrder = order === 'desc' ? -1 : 1
+
+    if (type === 'name') {
+      dogsData.sort((a, b) => {
+        return sortOrder * a.breed.localeCompare(b.breed)
+      })
+    } else if (type === 'subBreed') {
+      dogsData.sort((a, b) => {
+        if (a.subBreeds.length === b.subBreeds.length) {
+          return sortOrder * a.breed.localeCompare(b.breed) // Ordena por nombre si la cantidad de subrazas es la misma
         }
-        displayTable(dogsData);
+        return sortOrder * (a.subBreeds.length - b.subBreeds.length) // Ordena por cantidad de subrazas
+      })
     }
-    
-    
+    displayTable(dogsData)
+  }
 
-    function returnToMainPage() {
-        window.location.href = 'index.html'; // retorno a pagina principal
-    }
+  function returnToMainPage () {
+    window.location.href = 'index.html' // retorno a pagina principal
+  }
 
-    returnButton.addEventListener('click', returnToMainPage);
+  returnButton.addEventListener('click', returnToMainPage)
 
-    sortSelect.addEventListener('change', function () {
-        const selectedSort = sortSelect.value;
-        sortDataBy(selectedSort);
-    });
-});
-
+  sortSelect.addEventListener('change', function () {
+    const selectedSort = sortSelect.value
+    sortDataBy(selectedSort)
+  })
+})
