0b50d623b49ee878a5c027e317633baa0db15af7
Gabriel Arturo Felix Paez <gabriel_arturo01@hotmail.com>
2023-12-20 20:48:44 -0400

    Update CSS background color and add a win message.

--- Prueba-03/main.js
+++ Prueba-03/main.js
@@ -12,62 +12,58 @@ let timerInterval;
 let seconds = 0;
 
 function resetBoard() {
-  firstCard = null;
-  secondCard = null;
-  lockBoard = false;
+    firstCard = null;
+    secondCard = null;
+    lockBoard = false;
 }
-
 function checkForMatch() {
   const selectedCards = document.querySelectorAll('.flipped');
 
   if (selectedCards.length === 2) {
-    const [firstCard, secondCard] = selectedCards;
+      lockBoard = true; // Bloquea el tablero mientras se están evaluando las cartas
 
-    const firstEmoji = firstCard.querySelector('.backSide').textContent;
-    const secondEmoji = secondCard.querySelector('.backSide').textContent;
+      const [firstCard, secondCard] = selectedCards;
 
-    if (firstEmoji === secondEmoji) {
-      disableCards(firstCard, secondCard);
-    } else {
-      unflipCards(firstCard, secondCard);
-    }
+      const firstEmoji = firstCard.querySelector('.backSide').textContent;
+      const secondEmoji = secondCard.querySelector('.backSide').textContent;
+
+      if (firstEmoji === secondEmoji) {
+          disableCards(firstCard, secondCard);
+      } else {
+          setTimeout(() => {
+              unflipCards(firstCard, secondCard);
+          }, 1000); // Retrasa el volteo de las cartas no coincidentes
+      }
   }
 }
+
 
 function disableCards(firstCard, secondCard) {
   firstCard.removeEventListener('click', handleCardClick);
   secondCard.removeEventListener('click', handleCardClick);
 
+  firstCard.classList.add('matched');
+  secondCard.classList.add('matched');
+
   matchedPairs += 2;
   moves++;
 
   if (matchedPairs === cards.length) {
-    clearInterval(timerInterval);
-    displayWinMessage();
+      clearInterval(timerInterval);
+      displayWinMessage();
   }
 
   resetBoard();
 }
 
-function unflipCards(firstCard, secondCard) {
-  lockBoard = true;
-
-  setTimeout(() => {
-    firstCard.classList.remove('flipped');
-    secondCard.classList.remove('flipped');
-
-    lockBoard = false;
-  }, 1000);
-}
-
 function handleCardClick() {
-  if (lockBoard || this === firstCard) return;
+  if (lockBoard || this === firstCard || this.classList.contains('matched')) return;
 
   this.classList.add('flipped');
 
   if (!firstCard) {
-    firstCard = this;
-    return;
+      firstCard = this;
+      return;
   }
 
   secondCard = this;
@@ -77,94 +73,95 @@ function handleCardClick() {
   checkForMatch();
 }
 
+
 function createCard(emoji) {
-  const card = document.createElement('div');
-  card.classList.add('myCard');
+    const card = document.createElement('div');
+    card.classList.add('myCard');
 
-  const innerCard = document.createElement('div');
-  innerCard.classList.add('innerCard');
+    const innerCard = document.createElement('div');
+    innerCard.classList.add('innerCard');
 
-  const frontSide = document.createElement('div');
-  frontSide.classList.add('frontSide');
-  frontSide.textContent = '🃏';
+    const frontSide = document.createElement('div');
+    frontSide.classList.add('frontSide');
+    frontSide.textContent = '⚛️';
 
-  const backSide = document.createElement('div');
-  backSide.classList.add('backSide');
-  backSide.textContent = emoji;
+    const backSide = document.createElement('div');
+    backSide.classList.add('backSide');
+    backSide.textContent = emoji;
 
-  innerCard.appendChild(frontSide);
-  innerCard.appendChild(backSide);
-  card.appendChild(innerCard);
+    innerCard.appendChild(frontSide);
+    innerCard.appendChild(backSide);
+    card.appendChild(innerCard);
 
-  card.addEventListener('click', handleCardClick);
+    card.addEventListener('click', handleCardClick);
 
-  return card;
+    return card;
 }
 
 function startGame(difficulty) {
-  let emojis;
+    let emojis;
 
-  if (difficulty === 'easy') {
-    emojis = emojisEasy.concat(emojisEasy);
-  } else if (difficulty === 'medium') {
-    emojis = emojisMedium.concat(emojisMedium);
-  } else if (difficulty === 'hard') {
-    emojis = emojisHard.concat(emojisHard);
-  }
+    if (difficulty === 'easy') {
+        emojis = emojisEasy.concat(emojisEasy);
+    } else if (difficulty === 'medium') {
+        emojis = emojisMedium.concat(emojisMedium);
+    } else if (difficulty === 'hard') {
+        emojis = emojisHard.concat(emojisHard);
+    }
 
-  cards = emojis.sort(() => Math.random() - 0.5);
+    cards = emojis.sort(() => Math.random() - 0.5);
 
-  initializeGame();
+    initializeGame();
 }
 
 function initializeGame() {
-  const gameBoard = document.querySelector('.game-board');
-  gameBoard.innerHTML = '';
+    const gameBoard = document.querySelector('.game-board');
+    gameBoard.innerHTML = '';
 
-  cards.forEach(emoji => {
-    const cardElement = createCard(emoji);
-    gameBoard.appendChild(cardElement);
-  });
+    cards.forEach(emoji => {
+        const cardElement = createCard(emoji);
+        gameBoard.appendChild(cardElement);
+    });
 
-  resetGameStats();
-  updateMoves();
-  startTimer();
+    resetGameStats();
+    updateMoves();
+    startTimer();
 }
 
 function resetGameStats() {
-  moves = 0;
-  matchedPairs = 0;
-  seconds = 0;
+    moves = 0;
+    matchedPairs = 0;
+    seconds = 0;
 }
 
 function updateMoves() {
-  const movesElement = document.querySelector('.moves');
-  movesElement.textContent = `Movimientos: ${moves}`;
+    const movesElement = document.querySelector('.moves');
+    movesElement.textContent = `Movimientos: ${moves}`;
 }
 
 function startTimer() {
-  const timerElement = document.querySelector('.timer');
-  timerInterval = setInterval(() => {
-    seconds++;
-    timerElement.textContent = `Tiempo: ${seconds} s`;
-  }, 1000);
+    const timerElement = document.querySelector('.timer');
+    timerInterval = setInterval(() => {
+        seconds++;
+        timerElement.textContent = `Tiempo: ${seconds} s`;
+    }, 1000);
 }
 
 function displayWinMessage() {
-  const winMessage = document.createElement('div');
-  winMessage.classList.add('win-message');
-  winMessage.textContent = `¡Has ganado en ${moves} movimientos y ${seconds} segundos!`;
+    const winMessage = document.createElement('div');
+    winMessage.classList.add('win-message');
+    winMessage.textContent = `¡Has ganado en ${moves} movimientos y ${seconds} segundos!`;
 
-  const gameBoard = document.querySelector('.game-board');
-  gameBoard.appendChild(winMessage);
+    const gameBoard = document.querySelector('.game-board');
+    gameBoard.appendChild(winMessage);
 }
 
 function resetGame() {
-  const gameBoard = document.querySelector('.game-board');
-  gameBoard.innerHTML = '';
+    const gameBoard = document.querySelector('.game-board');
+    gameBoard.innerHTML = '';
 
-  resetGameStats();
-  initializeGame();
+    resetGameStats();
+    initializeGame();
 }
 
 // Inicializar el juego al cargar la página
